78975f11bcb078fa6e6175d21070cec1b3846bfb
paramraghavan <paramraghavan@gmail.com>
2024-04-20 07:21:10 -0400

    remove header row

--- snowflake/dump_snowflake_tables.py
+++ snowflake/dump_snowflake_tables.py
@@ -32,7 +32,7 @@ try:
 
         # Write DataFrame to a pipe-delimited text file
         output_file = f"{table_name}.dat"
-        df.to_csv(output_file, sep='|', index=False)
+        df.to_csv(output_file, sep='|', header=False, index=False)
         print(f"Data for {table_name} written to {output_file}")
 finally:
     cursor.close()
